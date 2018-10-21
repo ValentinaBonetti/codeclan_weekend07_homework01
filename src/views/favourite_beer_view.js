5b8d39040f19048cdc89100a4fb0856df2c22a10
Valentina Bonetti <valentina.bonetti@strath.ac.uk>
2018-10-21 10:00:31 +0100

    Add a bit of style to the page

--- src/views/favourite_beer_view.js
+++ src/views/favourite_beer_view.js
@@ -5,6 +5,7 @@ const FavouriteBeerView = function () {
 FavouriteBeerView.prototype.createBeerView = function (beer) {
   const beerView = document.createElement('img');
   beerView.classList.add('favourite-beer-image');
+  beerView.id = `image-beerid-${beer.id}`;
   beerView.src = beer.image_url;
   return beerView;
 };
