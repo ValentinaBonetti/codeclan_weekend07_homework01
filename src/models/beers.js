d63edcae128007d50a3c96fce2fc04bb7a187994
Valentina Bonetti <valentina.bonetti@strath.ac.uk>
2018-10-22 07:20:58 +0100

    Change graph with histogram and give it basic style

--- src/models/beers.js
+++ src/models/beers.js
@@ -34,40 +34,48 @@ Beers.prototype.publishBeerByIndex = function (index) {
 };
 
 Beers.prototype.drawABVgraph = function () {
-
-  // Load the Visualization API and the corechart package.
-  google.charts.load('current', {'packages':['corechart']});
-
-  // Set a callback to run when the Google Visualization API is loaded.
+  google.charts.load("current", {packages:["corechart"]});
   google.charts.setOnLoadCallback(drawChart);
-
-  // Callback that creates and populates a data table,
-  // instantiates the pie chart, passes in the data and
-  // draws it.
   function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+      ['Dinosaur', 'Length'],
+      ['Acrocanthosaurus (top-spined lizard)', 12.2],
+      ['Albertosaurus (Alberta lizard)', 9.1],
+      ['Allosaurus (other lizard)', 12.2],
+      ['Apatosaurus (deceptive lizard)', 22.9],
+      ['Archaeopteryx (ancient wing)', 0.9],
+      ['Argentinosaurus (Argentina lizard)', 36.6],
+      ['Baryonyx (heavy claws)', 9.1],
+      ['Brachiosaurus (arm lizard)', 30.5],
+      ['Ceratosaurus (horned lizard)', 6.1],
+      ['Coelophysis (hollow form)', 2.7],
+      ['Compsognathus (elegant jaw)', 0.9],
+      ['Deinonychus (terrible claw)', 2.7],
+      ['Diplodocus (double beam)', 27.1],
+      ['Dromicelomimus (emu mimic)', 3.4],
+      ['Gallimimus (fowl mimic)', 5.5],
+      ['Mamenchisaurus (Mamenchi lizard)', 21.0],
+      ['Megalosaurus (big lizard)', 7.9],
+      ['Microvenator (small hunter)', 1.2],
+      ['Ornithomimus (bird mimic)', 4.6],
+      ['Oviraptor (egg robber)', 1.5],
+      ['Plateosaurus (flat lizard)', 7.9],
+      ['Sauronithoides (narrow-clawed lizard)', 2.0],
+      ['Seismosaurus (tremor lizard)', 45.7],
+      ['Spinosaurus (spiny lizard)', 12.2],
+      ['Supersaurus (super lizard)', 30.5],
+      ['Tyrannosaurus (tyrant lizard)', 15.2],
+      ['Ultrasaurus (ultra lizard)', 30.5],
+      ['Velociraptor (swift robber)', 1.8]]);
 
-    // Create the data table.
-    var data = new google.visualization.DataTable();
-    data.addColumn('string', 'Topping');
-    data.addColumn('number', 'Slices');
-    data.addRows([
-      ['Mushrooms', 3],
-      ['Onions', 1],
-      ['Olives', 1],
-      ['Zucchini', 1],
-      ['Pepperoni', 2]
-    ]);
+    var options = {
+      title: 'General distribution of ABV (Alcohol By Volume)',
+      legend: { position: 'none' },
+    };
 
-    // Set chart options
-    var options = {'title':'How Much Pizza I Ate Last Night',
-                   'width':400,
-                   'height':300};
-
-    // Instantiate and draw our chart, passing in some options.
-    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+    var chart = new google.visualization.Histogram(document.getElementById('chart_div'));
     chart.draw(data, options);
   }
-
 };
 
 module.exports = Beers;
