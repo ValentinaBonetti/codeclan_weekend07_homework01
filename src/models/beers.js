9ef0e2310df1a5061bf8beccbe26674ca21599db
Valentina Bonetti <valentina.bonetti@strath.ac.uk>
2018-10-22 07:11:25 +0100

    Add a random pie chart to bottom of page to try out google chart library

--- src/models/beers.js
+++ src/models/beers.js
@@ -15,8 +15,8 @@ Beers.prototype.bindEvents = function () {
     const selectedBeerIndex = this.beersData.filter(beer => {return beer.food_pairing.includes(selectedFood)}).map(beer => beer.id);
     this.publishBeerByIndex(selectedBeerIndex);
   })
+  this.drawABVgraph();
 
-  // json.filter(beer => {return beer.food_pairing.includes("Tomato and rosemary palmiers")}).map(beer => beer.name)
 };
 
 Beers.prototype.getData = function () {
@@ -31,6 +31,43 @@ Beers.prototype.getData = function () {
 Beers.prototype.publishBeerByIndex = function (index) {
   const selectedBeer = this.beersData.filter(beer => {return beer.id == index});
   PubSub.publish('Beers:selected-beer',selectedBeer[0])
+};
+
+Beers.prototype.drawABVgraph = function () {
+
+  // Load the Visualization API and the corechart package.
+  google.charts.load('current', {'packages':['corechart']});
+
+  // Set a callback to run when the Google Visualization API is loaded.
+  google.charts.setOnLoadCallback(drawChart);
+
+  // Callback that creates and populates a data table,
+  // instantiates the pie chart, passes in the data and
+  // draws it.
+  function drawChart() {
+
+    // Create the data table.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Topping');
+    data.addColumn('number', 'Slices');
+    data.addRows([
+      ['Mushrooms', 3],
+      ['Onions', 1],
+      ['Olives', 1],
+      ['Zucchini', 1],
+      ['Pepperoni', 2]
+    ]);
+
+    // Set chart options
+    var options = {'title':'How Much Pizza I Ate Last Night',
+                   'width':400,
+                   'height':300};
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+  }
+
 };
 
 module.exports = Beers;
